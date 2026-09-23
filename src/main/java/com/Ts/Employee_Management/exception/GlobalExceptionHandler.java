b30b56f0f8b3316885ae2b74f5b2095d9ba0bbfe
Mansvi Rajput <mansvirajput488@gmail.com>
2026-09-23 17:08:15 +0530

    Done 1

--- src/main/java/com/Ts/Employee_Management/exception/GlobalExceptionHandler.java
+++ src/main/java/com/Ts/Employee_Management/exception/GlobalExceptionHandler.java
@@ -3,11 +3,35 @@ package com.Ts.Employee_Management.exception;
 import com.Ts.Employee_Management.dto.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Object>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        Map<String, String> errorMap = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String msg = error.getDefaultMessage();
+            errorMap.put(fieldName, msg);
+        });
+
+        ApiResponse<Object> apiResponse = ApiResponse.builder()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .message("Validation failed")
+                .multiple(true)
+                .data(errorMap)
+                .build();
+
+        return new ResponseEntity<>(apiResponse, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(ConflictException.class)
     public ResponseEntity<ApiResponse<Object>>  handleConflictException(ConflictException e) {
